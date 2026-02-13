4362c15014679d08466ebd7ce678380f5217a507
Jh0wjso <jhonatansilverio1258@gmail.com>
2026-02-13 12:06:20 -0300

    feat: back-end code for extratc cv infos

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import OpenAI from "openai";
 import dotenv from "dotenv";
 import cors from "cors";
+import multer from "multer";
+import pdfParse from "pdf-parse-fork";
+import fs from "fs";
 
 dotenv.config();
 
@@ -9,9 +12,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const upload = multer({ dest: "uploads/" });
+
 const client = new OpenAI({
   baseURL: "https://router.huggingface.co/v1",
   apiKey: process.env.HF_TOKEN,
+});
+
+app.post("/upload", upload.single("resume"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const fileBuffer = fs.readFileSync(req.file.path);
+    const data = await pdfParse(fileBuffer);
+    fs.unlinkSync(req.file.path);
+
+    res.json({
+      text: data.text,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "PDF parsing failed" });
+  }
 });
 
 app.post("/analyze", async (req, res) => {
