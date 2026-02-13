44eb099ab432507ff499528d4dfa9d80d65315b6
Jh0wjso <jhonatansilverio1258@gmail.com>
2026-02-13 12:06:56 -0300

    feat: testing cv infos extracted

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import TestBackend from "./pages/TestBackend";
+import TestPDF from "./pages/TestPDF";
 
 const queryClient = new QueryClient();
 
@@ -16,8 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<TestPDF />} />
           <Route path="/ia_test" element={<TestBackend />} />
-          <Route path="/" element={<Index />} />
+          <Route path="/home" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
